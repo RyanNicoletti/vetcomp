908bfd4f98c02296fd037ee115502e1c0ef8f9b4
Ryan Nicoletti <r.nicoletti1@gmail.com>
2024-04-28 22:36:32 -0400

    add basic css to table

--- frontend/src/components/salarytable/SalaryTable.tsx
+++ frontend/src/components/salarytable/SalaryTable.tsx
@@ -9,8 +9,9 @@ import Paper from "@mui/material/Paper";
 import { useQuery } from "@tanstack/react-query";
 import { getAllSalaries } from "../../queries/salaryQueries";
 import ErrorBlock from "../ErrorBlock";
-import { Box } from "@mui/material";
 import { Salary } from "./types";
+import "./SalaryTable.css";
+import { Box } from "@mui/material";
 
 export default function SalaryTable() {
   const {
@@ -31,81 +32,81 @@ export default function SalaryTable() {
     return <div style={{ marginBottom: "300px" }}>Loading...</div>;
   }
 
-  console.log(salaryData);
-
   return (
-    <Box sx={{ width: "100%" }}>
-      <Paper sx={{ width: "100%", mb: 2 }}>
-        <TableContainer>
-          <Table sx={{ minWidth: 750 }} aria-labelledby="tableTitle">
-            <TableHead>
-              <TableRow>
-                <TableCell key="company-location" align={"left"}>
-                  <div>
-                    <p>Company</p>
-                    <span>Location | Date</span>
-                  </div>
-                </TableCell>
-                <TableCell key="type-of-practice" align={"left"}>
-                  <div>
-                    <p>Job Title</p>
-                    <span>Practice Type</span>
-                  </div>
-                </TableCell>
-                <TableCell key="years-of-experience" align={"right"}>
-                  <TableSortLabel>Years of Experience</TableSortLabel>
-                </TableCell>
-                <TableCell key="total-compensation" align={"right"}>
-                  <TableSortLabel>
+    <TableContainer>
+      <Table
+        sx={{
+          width: "50%",
+          mt: "100px",
+          mb: "100px",
+          mr: "auto",
+          ml: "auto",
+        }}
+        aria-labelledby="tableTitle">
+        <TableHead className="table-header">
+          <TableRow>
+            <TableCell key="company-location" align={"left"}>
+              <div>
+                <p>Company</p>
+                <span>Location | Date</span>
+              </div>
+            </TableCell>
+            <TableCell key="type-of-practice" align={"left"}>
+              <div>
+                <p>Job Title</p>
+                <span>Practice Type</span>
+              </div>
+            </TableCell>
+            <TableCell key="years-of-experience" align={"right"}>
+              <TableSortLabel>Years of Experience</TableSortLabel>
+            </TableCell>
+            <TableCell key="total-compensation" align={"right"}>
+              <TableSortLabel>
+                <div>
+                  <p>Total Compensation (USD)</p>
+                  <span>Base | Production (yr)</span>
+                </div>
+              </TableSortLabel>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {salaryData &&
+            salaryData.map((row: Salary, index: string | undefined) => {
+              return (
+                <TableRow className="table-rows">
+                  <TableCell
+                    id={index}
+                    scope="row"
+                    padding="none"
+                    component="th">
                     <div>
-                      <p>Total Compensation (USD)</p>
-                      <span>Base | Production (yr)</span>
+                      <p>{row.company}</p>
+                      <span>
+                        {row.location} | {row.created_at}
+                      </span>
                     </div>
-                  </TableSortLabel>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {salaryData &&
-                salaryData.map((row: Salary, index: string | undefined) => {
-                  return (
-                    <TableRow>
-                      <TableCell
-                        id={index}
-                        scope="row"
-                        padding="none"
-                        component="th">
-                        <div>
-                          <p>{row.company}</p>
-                          <span>
-                            {row.location} | {row.created_at}
-                          </span>
-                        </div>
-                      </TableCell>
-                      <TableCell align="right">
-                        <div>
-                          <p>{row.title}</p>
-                          <span>{row.type_of_practice}</span>
-                        </div>
-                      </TableCell>
-                      <TableCell align="right">
-                        {row.years_of_experience}
-                      </TableCell>
-                      <TableCell align="right">
-                        <div>
-                          <p>{row.total_compensation}</p>
-                          <span>
-                            {row.base_salary} | {row.average_annual_production}
-                          </span>
-                        </div>
-                      </TableCell>
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Paper>
-    </Box>
+                  </TableCell>
+                  <TableCell align="right">
+                    <div>
+                      <p>{row.title}</p>
+                      <span>{row.type_of_practice}</span>
+                    </div>
+                  </TableCell>
+                  <TableCell align="right">{row.years_of_experience}</TableCell>
+                  <TableCell align="right">
+                    <div>
+                      <p>{row.total_compensation}</p>
+                      <span>
+                        {row.base_salary} | {row.average_annual_production}
+                      </span>
+                    </div>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
