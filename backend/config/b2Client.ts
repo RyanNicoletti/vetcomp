3bcbc34d48436e8a3cec7975fcb3bdec0d089ecc
Ryan Nicoletti <r.nicoletti1@gmail.com>
2024-08-24 17:28:35 -0400

    remove unused dep

--- backend/config/b2Client.ts
+++ backend/config/b2Client.ts
@@ -1,17 +1,9 @@
 import { S3Client } from "@aws-sdk/client-s3";
-import { loadSharedConfigFiles } from "@smithy/shared-ini-file-loader";
-
-const awsProfile: string = "veterinarycomp";
-
-const sharedConfigFiles = await loadSharedConfigFiles();
-
-const config = sharedConfigFiles.configFile[awsProfile];
-const credentials = sharedConfigFiles.credentialsFile[awsProfile];
 
 export const b2Client = new S3Client({
   credentials: {
-    accessKeyId: credentials.aws_access_key_id!,
-    secretAccessKey: credentials.aws_secret_access_key!,
+    accessKeyId: process.env.B2_KEYID!,
+    secretAccessKey: process.env.B2_APPLICATION_KEY!,
   },
   region: process.env.B2_REGION,
   endpoint: process.env.B2_ENDPOINT_URL,
