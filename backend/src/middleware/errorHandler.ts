bc8802b1386b004f24e6f0c6e08be4add65abb36
Ryan Nicoletti <r.nicoletti1@gmail.com>
2024-09-15 21:12:57 -0400

    add asyncHandler and error handler and error class

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { HttpError } from "../errors/httpErrors";
+import z from "zod";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  console.error(err);
+
+  if (err instanceof HttpError) {
+    res.status(err.statusCode).json({
+      error: {
+        message: err.message,
+        status: err.statusCode,
+      },
+    });
+  } else if (err instanceof z.ZodError) {
+    res.status(400).json({
+      error: {
+        message: "Validation error",
+        details: err.errors,
+        status: 400,
+      },
+    });
+  } else {
+    res.status(500).json({
+      error: {
+        message: "An unexpected error occurred",
+        status: 500,
+      },
+    });
+  }
+};
