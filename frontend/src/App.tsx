46653f8a8fe75296968c9285101ba03d73227cc5
Ryan Nicoletti <r.nicoletti1@gmail.com>
2024-09-18 20:41:17 -0400

    fix about page import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ import { ProtectedRoute } from "./components/protectedroute/ProtectedRoute";
 import PolicyPage from "./pages/PolicyPage";
 import TermsOfService from "./components/tos-privacypolicy-cookies/termsofservice/TermsOfService";
 import Cookies from "./components/tos-privacypolicy-cookies/cookies/Cookies";
-import About from "./pages/About";
+import About from "./pages/About.tsx";
 import LandingPage from "./pages/LandingPage";
 import ForgotPasswordEmail from "./pages/ForgotPasswordEmail";
 import { PasswordReset } from "./pages/Passwordreset";
