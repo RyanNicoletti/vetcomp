43a7544a9246e54ad5789929eb5676b7c54eb8d1
Ryan Nicoletti <r.nicoletti1@gmail.com>
2025-03-30 21:22:04 -0400

    remove redundant headers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -41,15 +41,7 @@ app.use(
 );
 app.use(morgan("dev"));
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 app.use(express.json());
 
 let redisStore = new RedisStore({
